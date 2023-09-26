fcb760f145c97f3e703c1a8fbf175a57bcb67dc0
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2023-09-26 19:56:56 -0400

    created function to create a grid user input for grid size

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,7 +1,6 @@
 /*
-    **to be done later**
-    prompt user for a grid size 
-        set a variable to that input
+prompt user for a grid size 
+    set a variable to that input
 
 loop until counter var is as big as user input
     create a div child to .container
@@ -12,3 +11,21 @@ make sure that the total space of the grid stays the same, regardless of the siz
 when a mouse hovers one of the divs
     change color of the div
 */
+let gridSize = 0;
+while(gridSize < 16 || gridSize > 100) {
+    gridSize = prompt("Enter a size for the grid, less than 100 and greater than 16:");
+}
+
+
+createGrid(gridSize);
+
+
+function createGrid(size) {
+    for(let i = 0; i < size; i++){
+        const container = document.getElementById('container');
+        const newDiv = document.createElement("div");
+        newDiv.classList.add("grid");
+        container.appendChild(newDiv);
+    }
+}
+
